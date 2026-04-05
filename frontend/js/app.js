599fe512015a5ce5e7ebf9459067958c73fa0c43
mansicoding <110651819+mansicoding@users.noreply.github.com>
2026-04-05 18:06:44 +0530

    fix cors

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:5000/api';
+const API = "https://wanderlust-backend-ao05.onrender.com/api";
 let state = {
   user: null, token: null,
   destinations: [], wishlist: [],
