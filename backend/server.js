f61102c4cdd9457387d74a0325c62745d0b5bbac
mansicoding <110651819+mansicoding@users.noreply.github.com>
2026-04-05 17:38:42 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
